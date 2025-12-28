ba06bb9ba92d1e63bf7ba9a3b1f951f4d3d69fff
Nayeon07 <kimny5726@gmail.com>
2025-12-28 20:40:24 +0900

    Init: initial setting, global css

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './global.css'
+
+function App() {
+  return (
+    <main className='flex flex-col items-center justify-center w-screen h-screen'>
+      <h1 className='text-primary text-SB-30'>css example</h1>
+    </main>
+  );
+}
+
+export default App;
