7c60082c6a49664d55fcfa411789090c1b2b1a2f
SangHyun Jung <wjdtkdgus876@naver.com>
2026-01-06 00:54:34 +0900

    feat: Tanstack Query 주입

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// todo 라우터 구조 import문 들어올 예정
 import App from './App.tsx'
 import './styles/global.css'
 
+const queryClient = new QueryClient(); // 서버 데이터 저장소 -> 저장 / 캐싱 / 관리
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </QueryClientProvider>
 )
