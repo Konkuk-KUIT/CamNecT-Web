3a68e9f6b24ab235c8a993d07d421aa90cba2dd8
choeunseo <taskcho123@gmail.com>
2026-01-08 16:36:08 +0900

    feat: MainLayout 상단바, Community Tab구조 설계, InfoTab 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/Home/Home';
+import HomePage from './pages/HomePage/HomePage';
+import CommunityPage from './pages/Community/Community';
 import './styles/global.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/community',
+    element: <CommunityPage />,
   },
 ]);
 
