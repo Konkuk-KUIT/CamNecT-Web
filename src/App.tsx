384afe226b54867ba47fb11d4602ab8f9a3b88b7
SangHyun Jung <wjdtkdgus876@naver.com>
2026-01-13 02:32:19 +0900

    feat: App.tsx에 반응형 전역 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import './styles/global.css';
 // todo 4가지 공통 레이아웃 구조 (헤더 / 탭바 유무)
 function App() {
   return (
-    <div>
+    // 전역 레이아웃 적용 (반응형)
+    <div className="w-full max-w-[430px] mx-auto min-h-screen bg-white relative">
       <Outlet/>
     </div>
   );
