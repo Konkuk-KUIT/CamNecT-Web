53abae665145ca6059588420142bf5ad75264e9d
choeunseo <taskcho123@gmail.com>
2026-01-14 02:07:34 +0900

    refactor: 충돌방지 app파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage';
-import CommunityPage from './pages/Community/CommunityPage';
-import WritePage from './pages/Community/WritePage';
+import { Outlet } from 'react-router-dom';
 import './styles/global.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/community',
-    element: <CommunityPage />,
-  },
-  {
-    path: '/community/write',
-    element: <WritePage />,
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    // 전역 레이아웃 적용 (반응형)
+    <div className="w-full max-w-[430px] mx-auto min-h-screen bg-white relative">
+      <Outlet/>
+    </div>
+  );
 }
 
 export default App;
