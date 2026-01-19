916e62f6d6e325386168043c0481bd49f0d0a5f4
SangHyun Jung <wjdtkdgus876@naver.com>
2026-01-19 23:52:56 +0900

    feat: 처음 스플래시 화면 + 온보딩 화면 구현 (일부)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './styles/global.css';
 function App() {
   return (
     // 전역 레이아웃 적용 (반응형)
-    <div className="w-full max-w-[430px] mx-auto min-h-screen bg-white relative">
+    <div className="w-full max-w-[430px] mx-auto h-[100dvh] bg-white relative">
       <Outlet/>
     </div>
   );
--- src/pages/onboarding/OnboardingPage.tsx
+++ src/pages/onboarding/OnboardingPage.tsx
@@ -1,9 +1,26 @@
-import { EmptyLayout } from '../../layouts/EmptyLayout';
-
+import { useEffect, useState } from 'react';
+import Button from '../../components/Button';
+import ButtonWhite from '../../components/ButtonWhite';
+import { FirstSplashPage } from './FirstSplashPage';
 
 export const OnboardingPage = () => {
+
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowSplash(false);
+    }, 2000);
+  }, []);
+
+  if (showSplash) {
+    return <FirstSplashPage />;
+  }
+
   return (
-    <EmptyLayout>
-    </EmptyLayout>
+    <div className="absolute bottom-[120px] left-1/2 -translate-x-1/2 flex flex-col items-center gap-[10px]">
+      <Button label="로그인" />
+      <ButtonWhite label="회원가입" />
+    </div>
   );
 };
