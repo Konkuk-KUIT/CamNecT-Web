2972b5473a7b347eaacda892c816c56ab30da403
Nayeon07 <kimny5726@gmail.com>
2026-01-21 01:46:17 +0900

    feat: 다른 url로 이동 시 맨 위로 자동 스크롤

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,14 @@
+//다른 url로 이동했을 때 맨 위로 스크롤
+
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+export function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+}
