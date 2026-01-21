bd76414fb687c69a00786a921e46f2035983f2d7
SangHyun Jung <wjdtkdgus876@naver.com>
2026-01-21 23:42:55 +0900

    style: Button 공통 컴포넌트 반응형으로 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ const Button = ({ label, className = '', disabled, ...props }: ButtonProps) => {
   return (
     <button
       disabled={disabled}
-      className={`w-[325px] h-[50px] rounded-[25px] flex items-center justify-center transition-all duration-200 ease-out text-white text-SB-18 rotate-0
+      className={`max-w-[325px] w-full h-[50px] rounded-[25px] flex items-center justify-center transition-all duration-200 ease-out text-white text-SB-18 rotate-0
         ${disabled 
           ? 'bg-gray-150 cursor-not-allowed' 
           : 'bg-primary cursor-pointer hover:bg-green-100 hover:-translate-y-[2px] hover:shadow-[0_10px_24px_rgba(0,197,108,0.28)]'
