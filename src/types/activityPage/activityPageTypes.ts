048a53e83dd5587be7d909ce440bc0f311f6d7b2
Nayeon07 <kimny5726@gmail.com>
2026-02-03 14:13:06 +0900

    fix: activity 타입 수정

--- src/types/activityPage/activityPageTypes.ts
+++ src/types/activityPage/activityPageTypes.ts
@@ -1,10 +1,6 @@
 import { type Activity } from "../activity/activityTypes";
 import { type TeamPost } from "../team/teamTypes";
 
-export type ActivityTab = "club"|"study"|"contest"|"external"|"job"|"recruit";
-export type ActivityTag = "광고/마케팅"|"기획/아이디어"|"개발"|"디자인" |"기타";
-export type SortKey = "추천순"|"최신순"|"마감임박순";
-
 // // 공모전 공통 인터페이스
 // export interface Activity {
 //   id: string;          
@@ -20,13 +16,11 @@ export type SortKey = "추천순"|"최신순"|"마감임박순";
 //대외활동 데이터
 
 export interface ActivityListItem extends Activity{
-  tab: ActivityTab;
-
-  tags: ActivityTag[];
-  dDay?: number;
-  isBookmarked: boolean;
-
-  createdAt: string; //웹에 등록된 시간
+    authorId: string;
+    content?: string;
+    tags: string[];
+    createdAt: string;
+    isBookmarked?: boolean;
 }
 
 export interface ActivityDetail extends ActivityListItem {
@@ -48,7 +42,6 @@ export interface ActivityDetail extends ActivityListItem {
 
 //팀원 공고 데이터
 
-export type RecruitStatus = "OPEN"|"CLOSED";
 
 // // 팀원 모집 인터페이스
 // export interface TeamPost {
@@ -57,16 +50,13 @@ export type RecruitStatus = "OPEN"|"CLOSED";
 // }
 
 export interface TeamRecruitPost extends TeamPost{
-  activityId: string; // 어떤 활동에 붙은 팀원공고인지
-
-  authorName: string;
-
-  status: RecruitStatus; // 모집중/완료
-  pastDays: number; // -일 전
-  bookmarkCount: number;
-  commentCount: number;
-
-  isBookmarked: boolean;
+    activityId: string; // 어떤 활동에 붙은 팀원공고인지
+    authorId: string;
+    authorName: string;
+    contestName?: string;
+    recruitNow: boolean; // 모집중/완료
+    bookmarkCount: number;
+    createdAt: string;
 }
 
 export interface TeamRecruitDetail extends TeamRecruitPost{
@@ -101,12 +91,7 @@ export interface TeamRecruitComment {
 
 // 기본 UI
 export interface ActivityListUiState {
-  activeTab: ActivityTab;
-
-  selectedField: "전체" | ActivityTag;
   selectedHost: "전체" | "공공기관" | "기업" | "학교" | "동아리/커뮤니티" | "기타";
   selectedRegion: "전체"|"서울"|"경기"|"강원"|"충북"|"충남"|"전북"|"전남"|"경북"|"경남"|"부산";
-
-  sort: SortKey;
 }
 
