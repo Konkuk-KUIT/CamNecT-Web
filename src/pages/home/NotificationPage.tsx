cf90fbf2243adaf2d925977e05c35231f601fb9b
SangHyun Jung <wjdtkdgus876@naver.com>
2026-02-18 01:08:13 +0900

    feat: 알림 클릭 시 라우팅 구현

--- src/pages/home/NotificationPage.tsx
+++ src/pages/home/NotificationPage.tsx
@@ -1,16 +1,17 @@
-import { useEffect, useMemo, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { requestNotificationRead, requestNotifications } from '../../api/notifications';
+import PopUp from '../../components/Pop-up';
 import { HeaderLayout } from '../../layouts/HeaderLayout';
 import { MainHeader } from '../../layouts/headers/MainHeader';
+import { useAuthStore } from '../../store/useAuthStore';
+import { useNotificationStore } from '../../store/useNotificationStore';
 import {
   notificationIconAssets,
   type NotificationItem,
   type NotificationType,
 } from './notificationData';
-import { requestNotificationRead, requestNotifications } from '../../api/notifications';
-import PopUp from '../../components/Pop-up';
-import { useNotificationStore } from '../../store/useNotificationStore';
-import { useAuthStore } from '../../store/useAuthStore';
 import { mapNotificationResponseToItems } from './notificationMapper';
 
 type PopUpConfig = {
@@ -190,6 +191,7 @@ const renderIcon = (notification: NotificationItem) => {
 };
 
 export const NotificationPage = () => {
+  const navigate = useNavigate();
   const [popUpConfig, setPopUpConfig] = useState<PopUpConfig | null>(null);
   const [isErrorDismissed, setIsErrorDismissed] = useState(false);
   const userId = useAuthStore((state) => state.user?.id);
@@ -237,22 +239,20 @@ export const NotificationPage = () => {
 
     switch (notification.type) {
       case 'coffeeChatRequest':
-        // TODO: 커피챗 요청 - 해당하는 유저의 요청 수락 페이지로 이동
+        navigate('/chat/requests');
         break;
       case 'pointUse':
-        // TODO: 포인트 사용 - 포인트 내역 페이지 연결
+        // 포인트 사용 - 내역 페이지가 없음 클릭해도 이동 X
         break;
       case 'pointEarn':
-        // TODO: 포인트 적립 - 포인트 내역 페이지 연결
+        // 포인트 적립 - 내역 페이지가 없음 클릭해도 이동 X
         break;
       case 'commentAccepted':
-        // TODO: 댓글 채택 - 해당 게시물로 이동
-        break;
       case 'reply':
-        // TODO: 답글 - 해당하는 답글이 달린 게시물 연결
-        break;
       case 'comment':
-        // TODO: 댓글 - 해당하는 댓글이 달린 게시물 연결
+        if (notification.postId) {
+          navigate(`/community/post/${notification.postId}`);
+        }
         break;
       default:
         break;
